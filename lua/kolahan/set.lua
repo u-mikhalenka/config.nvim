8831591865c68f0f69e0756d05aa1427c3b73ac6
Vladimir Mikhalenko <mikhalenko@acdlabs.by>
2026-05-19 18:47:24 +0100

    reload on changes

--- lua/kolahan/set.lua
+++ lua/kolahan/set.lua
@@ -70,3 +70,29 @@ vim.api.nvim_create_autocmd("TextYankPost", {
         (vim.hl or vim.highlight).on_yank()
     end,
 })
+
+vim.opt.autoread = true
+vim.opt.updatetime = 1000
+
+vim.api.nvim_create_autocmd({
+    "FocusGained",
+    "BufEnter",
+    "CursorHold",
+    "CursorHoldI",
+    "TermClose",
+    "TermLeave",
+}, {
+    group = vim.api.nvim_create_augroup("kolahan_autoread", { clear = true }),
+    callback = function()
+        if vim.fn.mode() ~= "c" then
+            vim.cmd("checktime")
+        end
+    end,
+})
+
+vim.api.nvim_create_autocmd("FileChangedShellPost", {
+    group = vim.api.nvim_create_augroup("kolahan_file_changed", { clear = true }),
+    callback = function()
+        vim.notify("File reloaded from disk", vim.log.levels.INFO)
+    end,
+})
