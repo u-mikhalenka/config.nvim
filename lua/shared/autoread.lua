b29155a290643fb643be562ed3c7b160d7207326
Uladzimir Mikhalenka <u-mikhalenka.git@voblaka.org>
2026-09-04 22:00:09 +0100

    nvim 0.13 autoread

--- lua/shared/autoread.lua
+++ lua/shared/autoread.lua
@@ -2,30 +2,32 @@ local M = {}
 
 M.setup = function()
   vim.opt.autoread = true
-  vim.opt.updatetime = 1000
+  if vim.version().minor <= 11 then
+    vim.opt.updatetime = 1000
 
-  vim.api.nvim_create_autocmd({
-    "FocusGained",
-    "BufEnter",
-    "CursorHold",
-    "CursorHoldI",
-    "TermClose",
-    "TermLeave",
-  }, {
-    group = vim.api.nvim_create_augroup("personal_autoread", { clear = true }),
-    callback = function()
-      if vim.fn.mode() ~= "c" then
-        vim.cmd("checktime")
-      end
-    end,
-  })
+    vim.api.nvim_create_autocmd({
+      "FocusGained",
+      "BufEnter",
+      "CursorHold",
+      "CursorHoldI",
+      "TermClose",
+      "TermLeave",
+    }, {
+      group = vim.api.nvim_create_augroup("personal_autoread", { clear = true }),
+      callback = function()
+        if vim.fn.mode() ~= "c" then
+          vim.cmd("checktime")
+        end
+      end,
+    })
 
-  vim.api.nvim_create_autocmd("FileChangedShellPost", {
-    group = vim.api.nvim_create_augroup("personal_file_changed", { clear = true }),
-    callback = function()
-      vim.notify("File reloaded from disk", vim.log.levels.INFO)
-    end,
-  })
+    vim.api.nvim_create_autocmd("FileChangedShellPost", {
+      group = vim.api.nvim_create_augroup("personal_file_changed", { clear = true }),
+      callback = function()
+        vim.notify("File reloaded from disk", vim.log.levels.INFO)
+      end,
+    })
+  end
 end
 
 return M
